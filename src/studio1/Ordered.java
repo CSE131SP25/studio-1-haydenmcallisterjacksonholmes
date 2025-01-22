c3bb089eb308217efb1137966a2908d44dd2045c
haydenmcallister <haydenmcallister@localhost>
2025-01-22 15:31:02 -0600

    We did that assignment so good, hell to the yeah.

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,14 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		// 
+		if(x>y && y>z||x<y && y<z){
+			System.out.println("This sequence is good");
+		}
+		else {
+			System.out.println("This sequence is horrible. boo.");
+		}
+		
 	}
 
 }
